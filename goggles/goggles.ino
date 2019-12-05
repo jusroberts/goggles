ad3fb4f3a4b9ed0095bebea0ed5dbe97b812244e
Justin Roberts <justinroberts@pce-ga.com>
2019-12-04 21:49:19 -0500

    Single-color modes now works

--- goggles/goggles.ino
+++ goggles/goggles.ino
@@ -41,17 +41,23 @@ int previousSide = 0;
 int RIGHT_SIDE = 0;
 int LEFT_SIDE = 1;
 
-int MODE_ALTERNATING_RANDOM = 0;
-int MODE_ALTERNATING_RANDOM_FILL = 1;
-int MODE_SPIN_CCW = 2;
-int MODE_SPIN_CW = 3;
-int MODE_SPIN_UP = 4;
-int MODE_SPIN_DOWN = 5;
-int MODE_PULSE = 6;
-int MODE_PULSE_ALTERNATING = 7;
+const int MODE_ALTERNATING_RANDOM = 0;
+const int MODE_ALTERNATING_RANDOM_FILL = 1;
+const int MODE_SPIN_CCW = 2;
+const int MODE_SPIN_CW = 3;
+const int MODE_SPIN_UP = 4;
+const int MODE_SPIN_DOWN = 5;
+const int MODE_PULSE = 6;
+const int MODE_PULSE_ALTERNATING = 7;
 
-int num_modes = 8;
-int current_mode;
+int numModes = 8;
+int currentMode;
+
+int remainingIterations = 0;
+int pulseLength = 0;
+
+int leftPulse[16];
+int rightPulse[16];
 
 void setup(void) 
 {
@@ -59,19 +65,170 @@ void setup(void)
    strip.show();
    randomSeed(analogRead(1)); // MAKE SURE THIS IS A DIFFERENT PIN FROM YOUR DIGITAL OUT
    Serial.begin(9600);
-   setup_alternating_random_fill();
-   setupPulseAllAlternating(shortPulse);
+   setupNextMode();
 }
 
 // main processing loop
 void loop(void) 
 {
-   pulse(shortPulse);
-   strip.show();
+  runMode();
+  strip.show();
 }
 
-int leftPulse[16];
-int rightPulse[16];
+void runMode()
+{
+  switch (currentMode) {
+    case MODE_SPIN_CCW:
+    case MODE_SPIN_CW:
+    case MODE_SPIN_UP:
+    case MODE_SPIN_DOWN:
+      spin();
+      break;
+    case MODE_PULSE_ALTERNATING:
+    case MODE_PULSE:
+      pulse();
+      break;
+    case MODE_ALTERNATING_RANDOM:
+      if (timer() == 1){
+        alternatingRandom();
+      }
+      break;
+    case MODE_ALTERNATING_RANDOM_FILL:
+      if (timer() == 1){
+        alternatingRandomFill();
+      }
+      break;
+  }
+}
+
+void setupNextMode()
+{
+  currentMode = random(numModes);
+  setupMode();
+}
+
+void setupMode()
+{
+  remainingIterations = random(5)+5;
+  
+  turn_off();
+  
+  switch (currentMode) {
+    case MODE_SPIN_CCW:
+    case MODE_SPIN_CW:
+    case MODE_SPIN_UP:
+    case MODE_SPIN_DOWN:
+      pulseLength = 1 << (random(2) + 6);
+      spinSetup();
+      Serial.print(remainingIterations);
+      Serial.print(" Spin ");
+      Serial.print(pulseLength);
+      break;
+    case MODE_PULSE_ALTERNATING:
+      pulseLength = 1 << (random(2) + 7);
+      setupPulseAllAlternating();
+      Serial.print(remainingIterations);
+      Serial.print(" Pulse Alt ");
+      Serial.print(pulseLength);
+      break;
+    case MODE_PULSE:
+      pulseLength = 1 << (random(2) + 7);
+      setupPulseAll();
+      Serial.print(remainingIterations);
+      Serial.print(" Pulse Both ");
+      Serial.print(pulseLength);
+      break;
+    case MODE_ALTERNATING_RANDOM:
+      remainingIterations = random(20)+25;
+      timerMax = 1 << (random(2) + 7);
+      Serial.print(remainingIterations);
+      Serial.print(" Random ");
+      break;
+    case MODE_ALTERNATING_RANDOM_FILL:
+      setupAlternatingRandomFill();
+      timerMax = 1 << (random(2) + 7);
+      Serial.print(remainingIterations);
+      Serial.print(" Random Fill ");
+      break;
+  }
+  Serial.println("");
+}
+
+int leftDirection = 0;
+int rightDirection = 0;
+int leftSpinPos = 0;
+int rightSpinPos = 0;
+int tailLength = 0;
+void spinSetup() {
+  switch (currentMode) {
+    case MODE_SPIN_CCW:
+    leftDirection = 1;
+    rightDirection = 1;
+    break;
+    case MODE_SPIN_CW:
+    leftDirection = -1;
+    rightDirection = -1;
+    break;
+    case MODE_SPIN_UP:
+    leftDirection = 1;
+    rightDirection = -1;
+    break;
+    case MODE_SPIN_DOWN:
+    leftDirection = -1;
+    rightDirection = 1;
+    break;
+  }
+  
+  leftPulse[0] = 0;
+  rightPulse[0] = 0;
+  for (int i = 1; i < 16; i++) {
+    leftPulse[i] = -1;
+    rightPulse[i] = -1;
+  }
+
+  leftSpinPos = 0;
+  rightSpinPos = 0;
+  
+  
+  tailLength = 1 << (1 + random(4)); //bitshifting!
+}
+
+void spin()
+{
+  if (remainingIterations <= 0) {
+    endPulse(pulseLength);
+    return;
+  }
+  
+  for (int i = 0; i < 16; i++) {
+    leftPulse[i] = leftPulse[i] > pulseLength || leftPulse[i] < 0 ? -1 : leftPulse[i] + 1;
+    rightPulse[i] = rightPulse[i] > pulseLength || rightPulse[i] < 0 ? -1 : rightPulse[i] + 1;
+    
+    
+    setPulseDisplay(leftPulse[i], leftPixels[i], pulseLength);
+    setPulseDisplay(rightPulse[i], rightPixels[i], pulseLength);
+  }
+
+  if (leftPulse[leftSpinPos] > pulseLength / tailLength) {
+    leftSpinPos = (leftSpinPos + leftDirection) % 16;
+    rightSpinPos = (rightSpinPos + rightDirection) % 16;
+
+    if (leftSpinPos < 0) {
+      leftSpinPos = 15;
+    }
+    if (rightSpinPos < 0) {
+      rightSpinPos = 15;
+    }
+
+    leftPulse[leftSpinPos] = 0;
+    rightPulse[rightSpinPos] = 0;
+    
+    if (leftSpinPos == 0) {
+      remainingIterations--;
+    }
+  }
+}
+
 void setupPulseAll()
 {
   for (int i = 0; i < 16; i++) {
@@ -80,7 +237,7 @@ void setupPulseAll()
   }
 }
 
-void setupPulseAllAlternating(int pulseLength)
+void setupPulseAllAlternating()
 {
   for (int i = 0; i < 16; i++) {
     leftPulse[i] = 0;
@@ -88,18 +245,42 @@ void setupPulseAllAlternating(int pulseLength)
   }
 }
 
-
-
-void pulse(int pulseLength)
+void pulse()
 {
+  if (remainingIterations <= 0) {
+    endPulse(pulseLength);
+    return;
+  }
+  
   for (int i = 0; i < 16; i++) {
-    leftPulse[i] = leftPulse[i] > pulseLength ? 0 : leftPulse[i] + 1;
-    rightPulse[i] = rightPulse[i] > pulseLength ? 0 : rightPulse[i] + 1;
+    leftPulse[i] = (leftPulse[i] + 1) % pulseLength;
+    rightPulse[i] = (rightPulse[i] + 1) % pulseLength;
 
     setPulseDisplay(leftPulse[i], leftPixels[i], pulseLength);
     setPulseDisplay(rightPulse[i], rightPixels[i], pulseLength);
   }
+
+  if (leftPulse[0] == 0) {
+    remainingIterations--;
+  }
   
+}
+
+void endPulse(int pulseLength)
+{
+  int sum = 0;
+  for (int i = 0; i < 16; i++) {
+    leftPulse[i] = leftPulse[i] > pulseLength  || leftPulse[i] == 0 ? 0 : leftPulse[i] + 1;
+    rightPulse[i] = rightPulse[i] > pulseLength || rightPulse[i] == 0 ? 0 : rightPulse[i] + 1;
+
+    sum += leftPulse[i] + rightPulse[i];
+
+    setPulseDisplay(leftPulse[i], leftPixels[i], pulseLength);
+    setPulseDisplay(rightPulse[i], rightPixels[i], pulseLength);
+  }
+  if (sum == 0) {
+    setupNextMode();
+  }
 }
 
 void setPulseDisplay(int value, int pixelNum, int pulseLength) {
@@ -107,20 +288,27 @@ void setPulseDisplay(int value, int pixelNum, int pulseLength) {
   if (value < 0) {
     colorValue = 0;
   } else {
-    colorValue = 126 - (int)(cos(2.0 * Pi * ((float)value / (float)pulseLength)) * 126.0);
+    colorValue = getColorValue(value, pulseLength);
   }
-  Serial.print(colorValue);
-  Serial.print(" ");
-  Serial.println(value);
   int red = colorValue;
   int green = 0;
   int blue = 0;
   strip.setPixelColor(pixelNum, strip.Color(red, green, blue));
 }
 
+int getColorValue(int value, int pulseLength)
+{
+  return 126 - (int)(cos(2.0 * Pi * ((float)value / (float)pulseLength)) * 126.0);
+}
 
 void alternatingRandom()
 {
+  if (remainingIterations <= 0) {
+    setupNextMode();
+    return;
+  }
+  remainingIterations--;
+  
   turn_off();
   int lightToShow = get_pixel_based_on_previous_side();
   updatePreviousSide();
@@ -130,22 +318,27 @@ void alternatingRandom()
 int unusedRightPixels[16];
 int unusedLeftPixels[16]; 
 
-void setup_alternating_random_fill()
+void setupAlternatingRandomFill()
 {
   for (int i = 0; i < 16; i++) {
     unusedRightPixels[i] = rightPixels[i];
     unusedLeftPixels[i] = leftPixels[i];
   }
 }
-void alternating_random_fill()
+
+void alternatingRandomFill()
 {
+  if (remainingIterations <= 0) {
+    setupNextMode();
+    return;
+  }
   int rightCount = getUnusedCount(unusedRightPixels);
   int leftCount = getUnusedCount(unusedLeftPixels);
-  Serial.println(rightCount);
 
   if (rightCount == 0 && leftCount == 0) {
-    setup_alternating_random_fill();
-    turn_off();
+    setupAlternatingRandomFill();
+    turn_off(); //TODO: turn this into a pulse off
+    remainingIterations--;
   } else {
 
     int lightToShow = get_pixel_based_on_previous_side();
